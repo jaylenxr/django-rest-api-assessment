2be26545d4ea527c39dd363d4f4b48df6c675945
Jaylen <jayrw6@gmail.com>
2025-06-07 16:20:34 -0500

    minor changes to songs

--- tunaapi/views/song.py
+++ tunaapi/views/song.py
@@ -2,14 +2,14 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from tunaapi.models import Song, Artist
+from tunaapi.models import Song, Artist, Genre
 
 
 class SongView(ViewSet):
-    # GET REQUEST FOR SINGLE SONG
+    # GET REQUEST FOR SINGLE SONG WITH ARTIST AND GENRE DETAILS
     def retrieve(self, request, pk):
         song = Song.objects.get(pk=pk)
-        serializer = SongSerializer(song)
+        serializer = SingleSongSerializer(song)
         return Response(serializer.data)
 
     # GET REQUEST FOR ALL SONGS
@@ -40,7 +40,7 @@ class SongView(ViewSet):
         song.album = request.data["album"]
         song.length = request.data["length"]
 
-        artist= Artist.objects.get(pk=request.data["artist_id"])
+        artist = Artist.objects.get(pk=request.data["artist_id"])
         song.artist = artist
         song.save()
 
@@ -50,11 +50,32 @@ class SongView(ViewSet):
     def destroy(self, request, pk):
         song = Song.objects.get(pk=pk)
         song.delete()
-
         return Response(None, status=status.HTTP_204_NO_CONTENT)
 
-
 class SongSerializer(serializers.ModelSerializer):
+    artist_name = serializers.CharField(source='artist.name', read_only=True)
     class Meta:
         model = Song
-        fields = ('id', 'title', 'artist', 'album', 'length')
+        fields = ('id', 'title', 'artist_name', 'album', 'length')
+
+class SingleSongSerializer(serializers.ModelSerializer):
+    artist = serializers.SerializerMethodField()
+    genres = serializers.SerializerMethodField()
+    class Meta:
+        model = Song
+        fields = ('id', 'title', 'album', 'length', 'artist', 'genres')
+
+    def get_artist(self, obj):
+        #returns an artist's details
+        return {
+            "id": obj.artist.id,
+            "name": obj.artist.name,
+            "age": obj.artist.age,
+            "bio": obj.artist.bio
+        }
+
+    def get_genres(self, obj):
+        #obj parameter is representing a song instance (for ex. Diamonds)
+        genres = Genre.objects.filter(songgenre__song=obj)
+        # changes the genre query into a list of dictionaries
+        return [{"id": genre.id, "description": genre.description} for genre in genres]
