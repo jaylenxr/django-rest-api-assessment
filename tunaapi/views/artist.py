bc461dfa4a59697470b924dfb99ef60d3c071f44
Jaylen <jayrw6@gmail.com>
2025-06-07 15:58:48 -0500

    able to view songs associated with an artist

--- tunaapi/views/artist.py
+++ tunaapi/views/artist.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from tunaapi.models import Artist
+from tunaapi.models import Artist, Song
 
 
 class ArtistView(ViewSet):
@@ -10,7 +10,7 @@ class ArtistView(ViewSet):
     def retrieve(self, request, pk):
 
         artist = Artist.objects.get(pk=pk)
-        serializer = ArtistSerializer(artist)
+        serializer = SingleArtistSerializer(artist)
         return Response(serializer.data)
 
     # GET REQUEST FOR ALL ARTISTS
@@ -29,7 +29,7 @@ class ArtistView(ViewSet):
             bio=request.data["bio"]
     )
         serializer = ArtistSerializer(artist)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     # PUT REQUEST TO UPDATE ARTIST
     def update(self, request, pk):
@@ -47,9 +47,19 @@ class ArtistView(ViewSet):
         artist.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
 
-
-
 class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
         fields = ('id', 'name', 'age', 'bio')
+
+class SingleArtistSerializer(serializers.ModelSerializer):
+    songs = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Artist
+        fields = ('id', 'name', 'age', 'bio', 'songs')
+
+    def get_songs(self, obj):
+        # Get all songs by this artist
+        songs = Song.objects.filter(artist=obj)
+        return [{"id": song.id, "title": song.title, "album": song.album, "length": song.length} for song in songs]
